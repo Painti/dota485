510859769d5610201f66672ce1b6d2d5271cc714
Painti <kwaipaint001@hotmail.com>
2017-04-23 17:27:11 +0700

    version 1.0.0

--- config.js
+++ config.js
@@ -3,5 +3,9 @@ module.exports = {
   port : 80,
   fb_appid : 291888114572237,
   fb_secret : '27e4bc144068fcbc7f81a393d582c65f',
-  st_key : 'D66F1513084C6633AC47944E35FF8203'
+  st_key : 'D66F1513084C6633AC47944E35FF8203',
+  convertSteam64to32 : function(id64){
+      var id = Number(id64.substr(3));
+      return id - 61197960265728;
+  }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var config = require('../config');
 var session = require('express-session');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.locals.steam = req.session.steam
+  res.locals.steam.id32 = config.convertSteam64to32(req.session.steam.id)
   res.render('index', {
     title: 'Dota485'
   });
