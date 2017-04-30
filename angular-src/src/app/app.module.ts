57d86e71510fdd9f633ecd3cdd07aab5a1d29638
Painti <kwaipaint001@hotmail.com>
2017-04-30 20:15:01 +0700

    version 1.0.0

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -16,13 +16,15 @@ import { LoginComponent } from './components/group2/login/login.component';
 import { MatchComponent } from './components/group2/match/match.component';
 import { ProfileComponent } from './components/group2/profile/profile.component';
 import { AuthService } from './services/group2/auth.service';
+import { AuthGuard } from './guard/auth.guard';
+import { LoginGuard } from './guard/login.guard';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'hero', component: HeroComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent , canActivate: [LoginGuard]},
   { path: 'match', component: MatchComponent },
-  { path: 'profile', component: ProfileComponent }
+  { path: 'profile', component: ProfileComponent , canActivate: [AuthGuard]}
 ]
 
 @NgModule({
@@ -41,7 +43,7 @@ const appRoutes: Routes = [
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
