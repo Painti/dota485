ff53e0c7ef435725963e5d85727e2a0b52192137
winner0660 <winjibi_@hotmail.com>
2017-05-17 23:50:09 +0700

    version 1.0.0

--- angular-src/src/app/components/group1/hero/hero.component.ts
+++ angular-src/src/app/components/group1/hero/hero.component.ts
@@ -8,10 +8,22 @@ import { GetApiService } from '../../../services/get-api.service';
 })
 export class HeroComponent implements OnInit {
 
-  constructor(private api:GetApiService) { }
-  hero:Array<Object>
+  constructor(
+    private api:GetApiService) {  }
+    heros:Object
+
   ngOnInit() {
+    this.api.getHeroes().subscribe(data =>{
+      this.heros = data;
+    },
+    err =>{
+        console.log(err);
+        return false;
 
+  });
+
+}
+  getImage(hName){
+    return "http://cdn.dota2.com/apps/dota2/images/heroes/"+hName+"_lg.png"
   }
-
 }
