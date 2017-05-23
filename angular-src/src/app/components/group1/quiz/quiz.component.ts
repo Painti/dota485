c60eb23c7ab3c657caac0c18815a4c263c80e284
winner0660 <winjibi_@hotmail.com>
2017-05-23 18:39:52 +0700

    version 1.0.0

--- angular-src/src/app/components/group1/quiz/quiz.component.ts
+++ angular-src/src/app/components/group1/quiz/quiz.component.ts
@@ -15,20 +15,41 @@ export class QuizComponent implements OnInit {
   private route: ActivatedRoute
   ) { }
   itemlist:Object
+  itemComponent:any
+  x:number;
   ngOnInit() {
+    this.x=0;
+    this.itemComponent = []
     this.api.getItem().subscribe( data => {
-      this.itemlist = data;
+      for (let key in data.itemdata) {
+          if(data.itemdata[key].components!=null){
+            this.itemComponent.push(data.itemdata[key])
+          }
+      }
+      this.itemlist = data.itemdata;
+      this.x = Math.floor((Math.random() * this.itemComponent.length) )
     },
      err => {
        console.log(err);
        return false;
      });
+
+
+
   }
 
   getName(name){
     return name.replace("item_", "");
   }
 
+  getImage(hName) {
+    return "http://cdn.dota2.com/apps/dota2/images/heroes/" + hName + "_lg.png"
+  }
+
+  randomItem(){
+    console.log(JSON.stringify(this.itemComponent[this.x]))
+    return this.itemComponent[this.x]
+  }
 
 
 }
