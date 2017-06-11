792004a501dda918a284622d669c0734c25d2c36
boatbot12 <boat.bom@gmail.com>
2017-06-11 19:41:24 +0700

    version 1.0.0

--- angular-src/src/app/components/group2/profile/overview-player/overview-player.component.ts
+++ angular-src/src/app/components/group2/profile/overview-player/overview-player.component.ts
@@ -15,7 +15,6 @@ export class OverviewPlayerComponent implements OnInit {
   peer: Array<Object>;
   player: Array<Object>;
   hero_name:Array<Object>;
-  hero_stat:Array<any> ;
   heroes_img: Array<Object> ;
   wl_recentMatch: Array<String> ;
   win_rate: Object;
@@ -29,6 +28,24 @@ export class OverviewPlayerComponent implements OnInit {
   heal_avr: any;
   tw_damage_avr: any;
   duration_avr: any;
+  best_kill:any ;
+  best_kill_id:any ;
+  best_deaths:any ;
+  best_deaths_id:any ;
+  best_assits:any ;
+  best_assits_id:any ;
+  best_gpm:any ;
+  best_gpm_id:any ;
+  best_xpm:any ;
+  best_xpm_id:any ;
+  best_lh:any ;
+  best_lh_id:any ;
+  best_damage:any ;
+  best_damage_id:any ;
+  best_heal:any ;
+  best_heal_id:any ;
+  best_tw_damage:any ;
+  best_tw_damage_id:any ;
 
   constructor(
     private authService: AuthService
@@ -56,26 +73,21 @@ export class OverviewPlayerComponent implements OnInit {
           return false;
         });
 
-        this.authService.getHero_Stats().subscribe(data => {
-          this.hero_stat = data ;
-
-        },
-        err => {
-          console.log(err);
-          return false;
-        });
-
       this.authService.getRecentMatch(this.user['account_id']).subscribe(data => {
         this.match = data ;
         this.kills_avr = 0 ; this.deaths_avr = 0 ;this.assists_avr = 0 ;
         this.gold_avr = 0;this.xp_avr = 0 ; this.lh_avr = 0;
         this.damage_avr = 0 ; this.heal_avr = 0;this.tw_damage_avr = 0 ;
+        this.best_kill = 0 ;this.best_deaths = 0 ;this.best_assits = 0 ;
+        this.best_gpm = 0 ;this.best_xpm = 0 ;this.best_lh = 0 ;
+        this.best_damage = 0 ;this.best_heal = 0 ;this.best_tw_damage = 0 ;
 
         this.wl_recentMatch = [] ;
         this.hero_name = [] ;
+        var win = 0 ;var tw_damage = 0;var heal = 0;var damage = 0 ;
 
         for(let i = 0 ; i < data.length  ;i++){
-          this.kills_avr =  this.kills_avr + this.match[i]['kills'] ;
+          this.kills_avr += this.match[i]['kills'] ;
           this.deaths_avr += this.match[i]['deaths'] ;
           this.assists_avr += this.match[i]['assists'] ;
           this.gold_avr += this.match[i]['gold_per_min'] ;
@@ -87,46 +99,118 @@ export class OverviewPlayerComponent implements OnInit {
           if(this.match[i]['player_slot'] < 5 && this.match[i]['radiant_win'] == false ||
             this.match[i]['player_slot'] > 5 && this.match[i]['radiant_win'] == true){
             this.wl_recentMatch.push("lose") ;
-          }else this.wl_recentMatch.push("Win")  ;
+          }else {this.wl_recentMatch.push("Win"); win++ ;}
           this.match[i]['cut_name'] = this.match[i]['heroes_name'].replace('_',' ') ;
           if(this.match[i]['player_slot'] <= 5){
             this.match[i]['player_team'] = "Radiant" ;
           }else this.match[i]['player_team'] = "Dire" ;
-
           this.hero_name[i] = this.match[i]['heroes_name'].charAt(0).toUpperCase() + this.match[i]['heroes_name'].slice(1).replace('_',' ') ;
+          if(this.best_kill < this.match[i]['kills']){
+            this.best_kill = this.match[i]['kills'] ;
+            this.best_kill_id = i ;
+          }
+          if(this.best_deaths < this.match[i]['deaths']){
+            this.best_deaths = this.match[i]['deaths'] ;
+            this.best_deaths_id = i ;
+          }
+          if(this.best_assits < this.match[i]['assists']){
+            this.best_assits = this.match[i]['assists'] ;
+            this.best_assits_id = i ;
+          }
+          if(this.best_gpm < this.match[i]['gold_per_min']){
+            this.best_gpm = this.match[i]['gold_per_min'] ;
+            this.best_gpm_id = i ;
+          }
+          if(this.best_xpm < this.match[i]['xp_per_min']){
+            this.best_xpm = this.match[i]['xp_per_min'] ;
+            this.best_xpm_id = i ;
+          }
+          if(this.best_lh < this.match[i]['last_hits']){
+            this.best_lh = this.match[i]['last_hits'] ;
+            this.best_lh_id = i ;
+          }
+          if(damage < this.match[i]['hero_damage']){
+            damage = this.match[i]['hero_damage'] ;
+            this.best_damage_id = i ;
+          }
+          if(heal < this.match[i]['hero_healing']){
+            heal = this.match[i]['hero_healing'] ;
+            this.best_heal_id = i ;
+          }
+          if(tw_damage < this.match[i]['tower_damage']){
+            tw_damage = this.match[i]['tower_damage']
+            this.best_tw_damage_id = i ;
+          }
         }
+        win = win / 20 * 100;
         this.kills_avr /= data.length;
         this.deaths_avr /= data.length ;
         this.assists_avr /= data.length ;
         this.gold_avr /= data.length ;
         this.xp_avr /= data.length ;
         this.lh_avr /= data.length ;
-        this.damage_avr /= data.length ;
         this.heal_avr /= data.length ;
-        this.tw_damage_avr /= data.length ;
 
-        this.kills_avr = this.kills_avr.toFixed(0) ;
-        this.deaths_avr = this.deaths_avr.toFixed(0) ;
-        this.assists_avr = this.assists_avr.toFixed(0) ;
-        this.gold_avr = this.gold_avr.toFixed(0) ;
-        this.xp_avr = this.xp_avr.toFixed(0) ;
-        this.lh_avr = this.lh_avr.toFixed(0) ;
-        this.damage_avr = this.damage_avr.toFixed(0) ;
-        this.heal_avr = this.heal_avr.toFixed(0) ;
-        this.tw_damage_avr = this.tw_damage_avr.toFixed(0) ;
+        if(this.damage_avr >= 1000){
+          this.damage_avr /= data.length * 1000 ;
+          this.damage_avr = this.damage_avr.toFixed(1) ;
+          this.damage_avr = this.damage_avr + "K" ;
+        }
+        else{
+          this.damage_avr /= data.length ;
+          this.damage_avr = this.damage_avr.toFixed(0) ;
+        }
+
+        if(this.tw_damage_avr >= 1000){
+            this.tw_damage_avr /= data.length * 1000 ;
+            this.tw_damage_avr = this.tw_damage_avr.toFixed(1) ;
+            this.tw_damage_avr = this.tw_damage_avr + "K" ;
+        }
+        else {
+          this.tw_damage_avr /= data.length ;
+          this.tw_damage_avr = this.damage_avr.toFixed(0) ;
+        }
+
+        if(heal > 1000){
+          heal = heal /1000;
+          this.best_heal = heal.toFixed(1) +"K";
+        }else this.best_heal = heal ;
+
+        if(damage > 1000){
+          damage = damage /1000;
+          this.best_damage = damage.toFixed(1) +"K";
+        }else this.best_damage = damage ;
+
+        if(tw_damage > 1000){
+          tw_damage = tw_damage /1000;
+          this.best_tw_damage = tw_damage.toFixed(1) +"K";
+        }else this.best_tw_damage = tw_damage ;
 
 
-      },
-      err => {
-        console.log(err);
-        return false;
-      });
+        if(data.length == 0){
+          this.kills_avr = 0 ;
+          this.deaths_avr = 0 ;
+          this.assists_avr = 0 ;
+          this.gold_avr = 0 ;
+          this.xp_avr = 0 ;
+          this.lh_avr = 0 ;
+          this.damage_avr = 0 ;
+          this.heal_avr = 0 ;
+          this.tw_damage_avr = 0 ;
+          this.win_rate = 0 ;
+        }else{
+          this.kills_avr = this.kills_avr.toFixed(0) ;
+          this.deaths_avr = this.deaths_avr.toFixed(0) ;
+          this.assists_avr = this.assists_avr.toFixed(0) ;
+          this.gold_avr = this.gold_avr.toFixed(0) ;
+          this.xp_avr = this.xp_avr.toFixed(0) ;
+          this.lh_avr = this.lh_avr.toFixed(0) ;
+          this.heal_avr = this.heal_avr.toFixed(0) ;
+          this.win_rate = win.toFixed(0) ;
 
-      this.authService.getWinAndLose(this.user['account_id']).subscribe(data => {
-        this.score = data ;
-        this.win_rate = (this.score['win'] / (this.score['win'] + this.score['lose'])) * 100 ;
-        var st_win_rate = JSON.stringify(this.win_rate);
-        this.win_rate = parseFloat(st_win_rate).toFixed(1) ;
+
+
+        }
       },
       err => {
         console.log(err);
@@ -161,9 +245,50 @@ export class OverviewPlayerComponent implements OnInit {
     return "https://api.opendota.com/apps/dota2/images/heroes/"+name+"_sb.png" ;
   }
 
+  getImageIcon(name){
+    return "https://api.opendota.com/apps/dota2/images/heroes/"+name+"_icon.png";
+  }
+
   getName(name){
     name = name.replace('_',' ') ;
     return name.charAt(0).toUpperCase()+name.slice(1);
+  }
+
+  getTime(time: number) {
+    let min = Math.floor(time / 60);
+    let sec = time % 60;
+    let sec_str: String;
+    if (sec < 10) {
+      sec_str = '0' + sec;
+    } else {
+      sec_str = '' + sec;
+    }
+    return min + ':' + sec_str;
+  }
+
+  getTimeAgo(previous: number) {
+    let msPerMinute = 60 * 1000;
+    let msPerHour = msPerMinute * 60;
+    let msPerDay = msPerHour * 24;
+    let msPerMonth = msPerDay * 30;
+    let msPerYear = msPerDay * 365;
+
+    let current = Date.now();
+    let elapsed = current - previous * 1000;
+
+    if (elapsed < msPerMinute) {
+      return Math.round(elapsed / 1000) + ' seconds ago';
+    } else if (elapsed < msPerHour) {
+      return Math.round(elapsed / msPerMinute) + ' minutes ago';
+    } else if (elapsed < msPerDay) {
+      return Math.round(elapsed / msPerHour) + ' hours ago';
+    } else if (elapsed < msPerMonth) {
+      return Math.round(elapsed / msPerDay) + ' days ago';
+    } else if (elapsed < msPerYear) {
+      return Math.round(elapsed / msPerMonth) + ' months ago';
+    } else {
+      return Math.round(elapsed / msPerYear) + ' years ago';
+    }
   }
 
 }
--- angular-src/src/app/components/group2/profile/peers-player/peers-player.component.ts
+++ angular-src/src/app/components/group2/profile/peers-player/peers-player.component.ts
@@ -21,6 +21,17 @@ export class PeersPlayerComponent implements OnInit {
 
       this.authService.getPeer(this.user['account_id']).subscribe(data => {
         this.peer = data ;
+        for(let i = 0 ; i < data.length  ;i++){
+          var percent = data[i]['win'] / data[i]['with_games'] *100 ;
+          var percent_against = data[i]['against_win'] / data[i]['against_games'] *100 ;
+          var avr_gpm = data[i]['with_gpm_sum'] / data[i]['with_games'] ;
+          var avr_xpm = data[i]['with_xpm_sum'] / data[i]['with_games']   ;
+          this.peer[i]['win_percentage'] = percent.toFixed(2);
+          if( data[i]['against_games'] == 0 )this.peer[i]['win_against_percentage'] = 0 ;
+          else this.peer[i]['win_against_percentage'] = percent_against.toFixed(2);
+          this.peer[i]['avr_gpm'] = avr_gpm.toFixed(0) ;
+          this.peer[i]['avr_xpm'] = avr_xpm.toFixed(0) ;
+        }
 
       },
       err => {
