139ba3c38e294f8c20998fef6806705b3b68f855
Painti <kwaipaint001@hotmail.com>
2017-06-14 23:03:06 +0700

    version 1.0.0

--- angular-src/src/app/components/group2/match/match-detail/benchmarks/benchmarks.component.ts
+++ angular-src/src/app/components/group2/match/match-detail/benchmarks/benchmarks.component.ts
@@ -12,28 +12,17 @@ export class BenchmarksComponent implements OnInit {
 
   constructor(
     private communicate: CommunicateService,
-<<<<<<< HEAD
     private slimLoadingBarService: SlimLoadingBarService
   ) { }
-=======
-    private slimLoadingBarservice: SlimLoadingBarService) { }
->>>>>>> 7f23607b8f3b2ea294847287a500621915faf79b
 
   match: Object;
   subscription: Subscription;
 
   ngOnInit() {
-<<<<<<< HEAD
     this.slimLoadingBarService.start();
     this.subscription = this.communicate.getMatch$.subscribe(match => {
       this.match = match;
       this.slimLoadingBarService.complete();
-=======
-    this.slimLoadingBarservice.start();
-    this.subscription = this.communicate.getMatch$.subscribe(match => {
-      this.match = match;
-      this.slimLoadingBarservice.complete();
->>>>>>> 7f23607b8f3b2ea294847287a500621915faf79b
     });
   }
 
